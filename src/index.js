905140d521a2e81f86ba8e249e70972427b72e9d
Sumeet <sumeetp987@gmail.com>
2024-07-07 18:22:58 +0530

    made some corrections for deployment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import {appRouter} from "./App"
-import appStore from './redux/appStore';
+import {appRouter} from "./App.js"
+import appStore from './redux/appStore.js';
 import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
